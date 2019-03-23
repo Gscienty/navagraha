7c088ee4018751308a327140d5f01120541247c7
Gscienty <gaoxiaochuan@hotmail.com>
2019-03-23 15:04:23 +0800

    add objectMeta model

---
+++ include/kubecli/object_meta.hpp
@@ -0,0 +1,55 @@
+#ifndef _NAVAGRAHA_KUBECTL_OBJECT_META_H
+#define _NAVAGRAHA_KUBECTL_OBJECT_META_H
+
+#include "extensions/field.hpp"
+#include "extensions/object_wrapper.hpp"
+#include "extensions/list_wrapper.hpp"
+#include "kubecli/time.hpp"
+#include "kubecli/initializers.hpp"
+
+namespace navagraha {
+namespace kubectl {
+
+extern char OBJECT_META_ANNOTATIONS[];
+extern char OBJECT_META_CLUSTER_NAME[];
+extern char OBJECT_META_CREATION_TIMESTAMP[];
+extern char OBJECT_META_DELETION_GRACE_PERIOD_SECONDS[];
+extern char OBJECT_META_DELETION_TIMESTAMP[];
+extern char OBJECT_META_FINALIZERS[];
+extern char OBJECT_META_GENERATE_NAME[];
+extern char OBJECT_META_GENERATION[];
+extern char OBJECT_META_INITIALIZERS[];
+extern char OBJECT_META_LABELS[];
+extern char OBJECT_META_NAME[];
+extern char OBJECT_META_NAMESPACE[];
+extern char OBJECT_META_OWNER_REFERENCES[];
+extern char OBJECT_META_RESOURCE_VERSION[];
+extern char OBJECT_META_SELF_LINK[];
+extern char OBJECT_META_UID[];
+
+class object_meta {
+public:
+    extensions::field<extensions::object_wrapper, OBJECT_META_ANNOTATIONS> annotations;
+    extensions::field<std::string, OBJECT_META_CLUSTER_NAME> cluster_name;
+    extensions::field<time, OBJECT_META_CREATION_TIMESTAMP> creation_timestamp;
+    extensions::field<int, OBJECT_META_DELETION_GRACE_PERIOD_SECONDS> deletion_grace_period_seconds;
+    extensions::field<time, OBJECT_META_DELETION_TIMESTAMP> deletion_timestamp;
+    extensions::field<extensions::list_wrapper, OBJECT_META_FINALIZERS> finalizers;
+    extensions::field<std::string, OBJECT_META_GENERATE_NAME> generate_name;
+    extensions::field<int, OBJECT_META_GENERATION> generation;
+    extensions::field<initializers, OBJECT_META_INITIALIZERS> inits;
+    extensions::field<extensions::object_wrapper, OBJECT_META_LABELS> labels;
+    extensions::field<std::string, OBJECT_META_NAME> name;
+    extensions::field<std::string, OBJECT_META_NAMESPACE> space;
+    extensions::field<extensions::list_wrapper, OBJECT_META_OWNER_REFERENCES> owner_references;
+    extensions::field<std::string, OBJECT_META_RESOURCE_VERSION> resource_version;
+    extensions::field<std::string, OBJECT_META_SELF_LINK> self_link;
+    extensions::field<std::string, OBJECT_META_UID> uid;
+
+    static void serialize(object_meta & obj, std::ostringstream & str);
+};
+
+}
+}
+
+#endif
---
+++ kubectl/object_meta.cc
@@ -0,0 +1,54 @@
+#include "kubecli/object_meta.hpp"
+#include <list>
+#include <functional>
+#include <algorithm>
+
+namespace navagraha {
+namespace kubectl {
+
+char OBJECT_META_ANNOTATIONS[] = "annotations";
+char OBJECT_META_CLUSTER_NAME[] = "clusterName";
+char OBJECT_META_CREATION_TIMESTAMP[] = "creationTimestamp";
+char OBJECT_META_DELETION_GRACE_PERIOD_SECONDS[] = "deletionGracePeriodSeconds";
+char OBJECT_META_DELETION_TIMESTAMP[] = "deletionTimestamp";
+char OBJECT_META_FINALIZERS[] = "finalizers";
+char OBJECT_META_GENERATE_NAME[] = "generateName";
+char OBJECT_META_GENERATION[] = "generation";
+char OBJECT_META_INITIALIZERS[] = "initializers";
+char OBJECT_META_LABELS[] = "labels";
+char OBJECT_META_NAME[] = "name";
+char OBJECT_META_NAMESPACE[] = "namespace";
+char OBJECT_META_OWNER_REFERENCES[] = "ownerReferences";
+char OBJECT_META_RESOURCE_VERSION[] = "resourceVersion";
+char OBJECT_META_SELF_LINK[] = "selfLink";
+char OBJECT_META_UID[] = "uid";
+
+void object_meta::serialize(object_meta & obj, std::ostringstream & str)
+{
+    std::list<std::pair<bool, std::function<void (std::ostringstream &)>>> serializers = {
+        extensions::field_serializer(obj.annotations),
+        extensions::field_serializer(obj.cluster_name),
+        extensions::field_serializer(obj.creation_timestamp),
+        extensions::field_serializer(obj.deletion_grace_period_seconds),
+        extensions::field_serializer(obj.deletion_timestamp),
+        extensions::field_serializer(obj.finalizers),
+        extensions::field_serializer(obj.generate_name),
+        extensions::field_serializer(obj.generation),
+        extensions::field_serializer(obj.inits),
+        extensions::field_serializer(obj.labels),
+        extensions::field_serializer(obj.name),
+        extensions::field_serializer(obj.owner_references),
+        extensions::field_serializer(obj.resource_version),
+        extensions::field_serializer(obj.self_link),
+        extensions::field_serializer(obj.space),
+        extensions::field_serializer(obj.uid)
+    };
+
+    str.put('{');
+    extensions::fields_serialize(serializers, str);
+    str.put('}');
+
+}
+
+}
+}
