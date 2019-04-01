021ffdde0e8ed675e25eea4c6e0267dfda7377de
Gscienty <gaoxiaochuan@hotmail.com>
2019-04-01 16:56:25 +0800

    fix: change type name

--- extensions/link_serializer.cc
+++ extensions/link_serializer.cc
@@ -11,14 +11,14 @@ link_serializer::link_serializer(link_serializer_type type)
 
 void link_serializer::serialize(std::ostringstream & str)
 {
-    if (this->_type == link_serializer_type::link_serializer_type_list) {
+    if (this->_type == link_serializer_type::list) {
         str.put('[');
     }
     else {
         str.put('{');
     }
     fields_serialize(this->_serializers, str);
-    if (this->_type == link_serializer_type::link_serializer_type_list) {
+    if (this->_type == link_serializer_type::list) {
         str.put(']');
     }
     else {
