3eced96a4349e1c12d8f7b3c9a34772ddfddb4c0
Gscienty <gaoxiaochuan@hotmail.com>
2019-04-08 09:43:21 -0400

    fix: string deserialize

--- extensions/link_serializer.cc
+++ extensions/link_serializer.cc
@@ -9,21 +9,54 @@ link_serializer::link_serializer(link_serializer_type type)
 
 }
 
-void link_serializer::serialize(std::ostringstream & str)
+char link_serializer::begin_brackets() const {
+    if (this->_type == link_serializer_type::list) {
+        return '[';
+    }
+    else {
+        return '{';
+    }
+}
+
+char link_serializer::end_brackets() const
 {
     if (this->_type == link_serializer_type::list) {
-        str.put('[');
+        return ']';
     }
     else {
-        str.put('{');
+        return '}';
     }
+}
+
+void link_serializer::serialize(std::ostringstream & str)
+{
+    str.put(this->begin_brackets());
     fields_serialize(this->_serializers, str);
-    if (this->_type == link_serializer_type::list) {
-        str.put(']');
+    str.put(this->end_brackets());
+}
+
+void link_serializer::deserialize(std::istringstream & str)
+{
+    std::ostringstream key_str;
+    while (str.peek() != this->end_brackets()) {
+        key_str.str("");
+        while (str.peek() != '"' && str.peek() != this->end_brackets()) {
+            str.get();
+        }
+        if (str.peek() == this->end_brackets()) {
+            break;
+        }
+        str.get();
+        while(str.peek() != '"') {
+            key_str.put(str.get());
+        }
+
+        auto deserialize_iter = this->_deserializers.find(key_str.str());
+        if (deserialize_iter != this->_deserializers.end()) {
+            deserialize_iter->second(str);
+        }
     }
-    else {
-        str.put('}');
-    }
+    str.get();
 }
 
 }
--- include/extensions/link_serializer.hpp
+++ include/extensions/link_serializer.hpp
@@ -4,6 +4,7 @@
 #include <list>
 #include <functional>
 #include <sstream>
+#include <map>
 #include "extensions/field.hpp"
 
 namespace navagraha {
@@ -18,17 +19,22 @@ class link_serializer {
 private:
     link_serializer_type _type;
     std::list<std::pair<bool, std::function<void (std::ostringstream &)>>> _serializers;
+    std::map<std::string, std::function<void (std::istringstream &)>> _deserializers;
 public:
     link_serializer(link_serializer_type type = link_serializer_type::object);
 
     template <typename T_Field> link_serializer & add(T_Field & field)
     {
         this->_serializers.push_back(field_serializer(field));
-
+        this->_deserializers.insert(field_deserialize(field));
         return *this;
     }
 
+    char begin_brackets() const;
+    char end_brackets() const;
+
     void serialize(std::ostringstream & str);
+    void deserialize(std::istringstream & str);
 };
 
 }
