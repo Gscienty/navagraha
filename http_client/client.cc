14526de2796f8dcbfa12741b0181d50cf2169725
Gscienty <gaoxiaochuan@hotmail.com>
2019-05-13 09:49:16 -0400

    fix: curl payload and content-type: application/json

--- http_client/client.cc
+++ http_client/client.cc
@@ -59,14 +59,26 @@ std::string & client::curl_abstract_process(const std::string path, CURLoption o
 
 std::string & client::curl_abstract_process(const std::string path, const char * method)
 {
+    curl_slist * headers = NULL;
     const std::string uri = this->uri(path);
     curl_easy_setopt(this->curl, CURLOPT_URL, uri.c_str());
     curl_easy_setopt(this->curl, CURLOPT_WRITEDATA, this);
     curl_easy_setopt(this->curl, CURLOPT_WRITEFUNCTION, __write_cb);
     curl_easy_setopt(this->curl, CURLOPT_CUSTOMREQUEST, method);
 
+    if (this->payload.size() != 0)
+    {
+        headers = curl_slist_append(headers, "Content-Type: application/json; charset=UTF-8");
+        curl_easy_setopt(this->curl, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(this->curl, CURLOPT_POSTFIELDS, this->payload.c_str());
+        curl_easy_setopt(this->curl, CURLOPT_POSTFIELDSIZE, this->payload.size());
+    }
+
     curl_easy_perform(this->curl);
 
+    if (headers != NULL) {
+        curl_slist_free_all(headers);
+    }
     return this->result;
 }
 
