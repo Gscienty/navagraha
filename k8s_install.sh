8f09fbd9ee2e300c45db9de46aa7d62f49ff83f6
Gscienty <gaoxiaochuan@hotmail.com>
2019-05-13 12:24:47 -0400

    added k8s install shell

---
+++ k8s_install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+
+yum install -y kubelet kubeadm kubectl
+
+kubeadm config images list | sed -e 's/^/docker pull /g' -e 's#k8s.gcr.io#docker.io/mirrorgooglecontainers#g' | sh -x
+
+docker images |grep mirrorgooglecontainers |awk '{print "docker tag",$1":"$2,$1":"$2}' |sed -e 's/docker\.io\/mirrorgooglecontainers/k8s.gcr.io/2' |sh -x
+docker images |grep mirrorgooglecontainers |awk '{print "docker rmi """$1""":"""$2}' |sh -x
+docker pull coredns/coredns:1.2.6
+docker tag coredns/coredns:1.2.6 k8s.gcr.io/coredns:1.2.6
+docker rmi coredns/coredns:1.2.6
+
+swapoff -a
+setenforce 0
+
+systemctl start kubelet
+
+kubeadm init --pod-network-cidr=10.244.0.0/16
+
+sysctl net.bridge.bridge-nf-call-iptables=1
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/a70459be0084506e4ec919aa1c114638878db11b/Documentation/kube-flannel.yml
+
+# master
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
