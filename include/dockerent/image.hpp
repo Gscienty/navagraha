e3cb397693db4c192474c365491cabfda570562c
Gscienty <gaoxiaochuan@hotmail.com>
2019-05-15 03:50:04 -0400

    feat: add header

---
+++ include/dockerent/image.hpp
@@ -0,0 +1,43 @@
+#ifndef _NAVAGRAHA_DOCKERENT_IMAGE_H
+#define _NAVAGRAHA_DOCKERENT_IMAGE_H
+
+#include "extensions/field.hpp"
+#include "extensions/special_list.hpp"
+#include "extensions/serializer_helper.hpp"
+#include "extensions/serializable.hpp"
+#include <string>
+
+namespace navagraha {
+namespace dockerent {
+
+extern char IMAGE_ID[];
+extern char IMAGE_PARENT_ID[];
+extern char IMAGE_REPO_TAGS[];
+extern char IMAGE_REPO_DIGESTS[];
+extern char IMAGE_CREATED[];
+extern char IMAGE_SIZE[];
+extern char IMAGE_SHARED_SIZE[];
+extern char IMAGE_VIRTUAL_SIZE[];
+extern char IMAGE_LABELS[];
+extern char IMAGE_CONTAINERS[];
+
+class image : public extensions::serializer<image> {
+public:
+    extensions::field<std::string, IMAGE_ID> id;
+    extensions::field<std::string, IMAGE_PARENT_ID> parent_id;
+    extensions::field<extensions::special_list<std::string>, IMAGE_REPO_TAGS> repo_tags;
+    extensions::field<extensions::special_list<std::string>, IMAGE_REPO_DIGESTS> repo_digests;
+    extensions::field<int, IMAGE_CREATED> created;
+    extensions::field<int, IMAGE_SIZE> size;
+    extensions::field<int, IMAGE_SHARED_SIZE> shared_size;
+    extensions::field<int, IMAGE_VIRTUAL_SIZE> virtual_size;
+    extensions::field<extensions::common_object, IMAGE_LABELS> labels;
+    extensions::field<int, IMAGE_CONTAINERS> containers;
+
+    void bind(extensions::serializer_helper & helper);
+};
+
+}
+}
+
+#endif
---
+++ include/extensions/serializable.hpp
@@ -0,0 +1,54 @@
+#ifndef _NAVAGRAHA_KUBEENT_SERIALIZABLE_H
+#define _NAVAGRAHA_KUBEENT_SERIALIZABLE_H
+
+#include "extensions/serializer_helper.hpp"
+#include "extensions/field.hpp"
+#include <sstream>
+
+namespace navagraha {
+namespace extensions {
+
+template <typename T_Obj>
+class serializable {
+public:
+    T_Obj & serialize(std::ostringstream & str)
+    {
+        return *serializer_helper()
+            .serialize(&T_Obj::bind, reinterpret_cast<T_Obj *>(this), str);
+    }
+
+    T_Obj & deserialize(std::istringstream & str)
+    {
+        return *serializer_helper()
+            .deserialize(&T_Obj::bind, reinterpret_cast<T_Obj *>(this), str);
+    }
+
+    static T_Obj deserialize(std::string & payload)
+    {
+        if (payload.length() == 0) {
+            payload = "{}";
+        }
+        std::istringstream str(payload);
+        T_Obj obj;
+
+        obj.deserialize(str);
+        return obj;
+    }
+
+    abstract_object to_abstract()
+    {
+        return serializer_helper()
+            .to_abstract(&T_Obj::bind, reinterpret_cast<T_Obj *>(this));
+    }
+
+    static T_Obj to_special(abstract_object & obj)
+    {
+        return serializer_helper()
+            .to_special(&T_Obj::bind, T_Obj(), obj);
+    }
+};
+
+}
+}
+
+#endif
