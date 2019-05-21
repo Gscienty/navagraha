4b4074ebe57d972522038d779702b30fcd1b90a8
Gscienty <gaoxiaochuan@hotmail.com>
2019-05-21 14:17:16 -0400

    feat: add namespace list

--- include/cli/namespace_list.hpp
+++ include/cli/namespace_list.hpp
@@ -4,11 +4,22 @@
 #include "cli_arg/abstract_process.hpp"
 #include "cli_arg/arg.hpp"
 #include "http_client/curl_helper.hpp"
+#include "kubeent/namespace.hpp"
 
 namespace navagraha {
 namespace cli {
 
+extern char CLI_NAMESPACE_FLAG[];
+
 class namespace_list : public cli_arg::abstract_process<namespace_list> {
+private:
+    cli_arg::arg<CLI_NAMESPACE_FLAG, 0> list_flag;
+
+    void display_item(kubeent::namespace_ & namespace_);
+public:
+    virtual void bind(cli_arg::process_helper<namespace_list> & helper) override;
+    virtual bool satisfy() const override;
+    virtual int execute() override;
 
 };
 
