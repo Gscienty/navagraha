3efb4f40cdaa8e7aaa75cc76647be14a7d4b21cc
Gscienty <gaoxiaochuan@hotmail.com>
2019-06-02 13:16:42 -0400

    feat: add file secret

--- cli/secret_add.cc
+++ cli/secret_add.cc
@@ -2,8 +2,8 @@
 #include "cli/config.hpp"
 #include "kubeent/secret.hpp"
 #include "kube_api/secret.hpp"
+#include "extensions/base64.hpp"
 #include <algorithm>
-
 #include <iostream>
 
 namespace navagraha {
@@ -12,11 +12,13 @@ namespace cli {
 char CLI_SECRET_ADD[] = "add";
 char CLI_SECRET_ADD_NAMESPACE[] = "--namespace";
 char CLI_SECRET_ADD_VALUE[] = "--value";
+char CLI_SECRET_ADD_FILE[] = "--file";
 
 void secret_add::bind(cli_arg::process_helper<secret_add> & helper)
 {
     this->namespace_arg.require(this->add_arg);
     this->value_arg.require(this->add_arg);
+    this->file_arg.require(this->add_arg);
 
     helper
         .add(this->add_arg)
@@ -55,6 +57,15 @@ int secret_add::execute()
                   secret.string_data.get().values()[kv[0]] = kv[1];
                   });
 
+    std::for_each(this->file_arg.get_params().begin(),
+                  this->file_arg.get_params().end(),
+                  [&, this] (std::array<std::string, 2> & kv)
+                  {
+                  std::string content = this->get_file_content(kv[1]);
+                  std::string base64_content = extensions::base64_encode(content);
+                  secret.data.get().values()[kv[0]] = base64_content;
+                  });
+
     std::ostringstream str;
     secret.serialize(str);
     std::cout << str.str() << std::endl;
@@ -62,6 +73,21 @@ int secret_add::execute()
     helper.build<kube_api::secret>().create(namespace_, secret);
 
     return 0;
+}
+
+std::string secret_add::get_file_content(std::string & filename)
+{
+    std::ostringstream str;
+    char c;
+
+    FILE * file = fopen(filename.c_str(), "r");
+
+    while ((c = fgetc(file)) != EOF) {
+        str.put(c);
+    }
+
+    fclose(file);
+    return str.str();
 }
 
 }
--- include/cli/secret_add.hpp
+++ include/cli/secret_add.hpp
@@ -12,12 +12,16 @@ namespace cli {
 extern char CLI_SECRET_ADD[];
 extern char CLI_SECRET_ADD_NAMESPACE[];
 extern char CLI_SECRET_ADD_VALUE[];
+extern char CLI_SECRET_ADD_FILE[];
 
 class secret_add : public cli_arg::abstract_process<secret_add> {
 private:
     cli_arg::arg<CLI_SECRET_ADD, 1> add_arg;
     cli_arg::arg<CLI_SECRET_ADD_NAMESPACE, 1> namespace_arg;
     cli_arg::multi_arg<CLI_SECRET_ADD_VALUE, 2> value_arg;
+    cli_arg::multi_arg<CLI_SECRET_ADD_FILE, 2> file_arg;
+
+    std::string get_file_content(std::string & filename);
 
 public:
     virtual void bind(cli_arg::process_helper<secret_add> & helper) override;
