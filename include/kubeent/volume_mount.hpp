448d1f2283b7b719ddfbc78a3f75d3cd169780f5
Gscienty <gaoxiaochuan@hotmail.com>
2019-06-09 13:02:37 -0400

    fix: volume_mount add name and expr fields

--- include/kubeent/volume_mount.hpp
+++ include/kubeent/volume_mount.hpp
@@ -14,13 +14,16 @@ extern char VOLUME_MOUNT_MOUNT_PROPAGATION[];
 extern char VOLUME_MOUNT_NAME[];
 extern char VOLUME_MOUNT_READ_ONLY[];
 extern char VOLUME_MOUNT_SUB_PATH[];
+extern char VOLUME_MOUNT_SUB_PATH_EXPR[];
 
 class volume_mount : public extensions::serializable<volume_mount> {
 public:
     extensions::field<std::string, VOLUME_MOUNT_MOUNT_PATH> mount_path;
     extensions::field<std::string, VOLUME_MOUNT_MOUNT_PROPAGATION> mount_propagation;
+    extensions::field<std::string, VOLUME_MOUNT_NAME> name;
     extensions::field<bool, VOLUME_MOUNT_READ_ONLY> read_only;
     extensions::field<std::string, VOLUME_MOUNT_SUB_PATH> sub_path;
+    extensions::field<std::string, VOLUME_MOUNT_SUB_PATH_EXPR> sub_path_expr;
 
     void bind(extensions::serializer_helper & helper);
 };
--- kubeent/volume_mount.cc
+++ kubeent/volume_mount.cc
@@ -8,14 +8,17 @@ char VOLUME_MOUNT_MOUNT_PROPAGATION[] = "mountPropagation";
 char VOLUME_MOUNT_NAME[] = "name";
 char VOLUME_MOUNT_READ_ONLY[] = "readOnly";
 char VOLUME_MOUNT_SUB_PATH[] = "subPath";
+char VOLUME_MOUNT_SUB_PATH_EXPR[] = "subPathExpr";
 
 void volume_mount::bind(extensions::serializer_helper & helper)
 {
     helper
         .add(this->mount_path)
         .add(this->mount_propagation)
+        .add(this->name)
         .add(this->read_only)
-        .add(this->sub_path);
+        .add(this->sub_path)
+        .add(this->sub_path_expr);
 }
 
 }
