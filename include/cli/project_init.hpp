d4cf23f2ebb8a5acde4f02f29582ab9ff01a0854
Gscienty <gaoxiaochuan@hotmail.com>
2019-07-03 06:10:59 -0400

    refactor: change fields private

--- include/cli/project_init.hpp
+++ include/cli/project_init.hpp
@@ -11,7 +11,7 @@ extern char CLI_PROJECT_INIT_PATH[];
 extern char CLI_PROJECT_INIT_LANG[];
 
 class project_init : public cli_arg::abstract_process<project_init> {
-public:
+private:
     cli_arg::arg<CLI_PROJECT_INIT_PATH, 1> arg_path;
     cli_arg::arg<CLI_PROJECT_INIT_LANG, 1> arg_lang;
 
