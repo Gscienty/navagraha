9ba82b6c7f5a7811a3817053099cb62f29ba2cfc
高小川 <gaoxiaochuan@hotmail.com>
2019-07-06 11:49:26 +0800

    Update k8s_install.sh

--- k8s_install.sh
+++ k8s_install.sh
@@ -25,11 +25,11 @@ setenforce 0
 
 systemctl start kubelet
 
-chown $(id -u):$(id -g) $HOME/.kube/config
-
 kubeadm init --pod-network-cidr=10.244.0.0/16
 
 sysctl net.bridge.bridge-nf-call-iptables=1
+
+chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/a70459be0084506e4ec919aa1c114638878db11b/Documentation/kube-flannel.yml
 
