a4401818ce0114e2c68d980f98762e0f149f6071
Gscienty <gaoxiaochuan@hotmail.com>
2019-07-23 06:24:40 -0400

    feat: supported chunked

--- http_client/client.cc
+++ http_client/client.cc
@@ -4,7 +4,7 @@
 namespace navagraha {
 namespace http_client {
 
-static size_t __write_cb(void * ptr, size_t size, size_t nitems, void * stream)
+static size_t __mono_write_cb(void * ptr, size_t size, size_t nitems, void * stream)
 {
     std::ostringstream * c = reinterpret_cast<std::ostringstream *>(stream);
     size_t readed_len = size;
@@ -13,6 +13,20 @@ static size_t __write_cb(void * ptr, size_t size, size_t nitems, void * stream)
     }
 
     c->write(reinterpret_cast<const char *>(ptr), readed_len);
+    return readed_len;
+}
+
+static size_t __chunk_write_cb(void * ptr, size_t size, size_t nitems, void * stream)
+{
+    std::function<void (std::string &)> * c = reinterpret_cast<std::function<void (std::string &)> *>(stream);
+    size_t readed_len = size;
+    if (readed_len < size * nitems) {
+        readed_len = size * nitems;
+    }
+
+    std::string chunk(reinterpret_cast<const char *>(ptr), reinterpret_cast<const char *>(ptr) + readed_len);
+    (*c)(chunk);
+
     return readed_len;
 }
 
@@ -35,15 +49,21 @@ std::string client::uri(const std::string path) const
     return this->host + path;
 }
 
-long client::curl_abstract_process(const std::string path, const char * method, std::ostringstream & oss)
+long client::curl_abstract_process(const std::string path, const char * method, std::ostringstream * oss)
 {
     curl_slist * headers = NULL;
     const std::string uri = this->uri(path);
     long response_code;
     curl_easy_setopt(this->curl, CURLOPT_URL, uri.c_str());
     
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &oss);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, __write_cb);
+    if (this->receive_cb) {
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &this->receive_cb);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, __chunk_write_cb);
+    }
+    else {
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, oss);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, __mono_write_cb);
+    }
 
     curl_easy_setopt(this->curl, CURLOPT_CUSTOMREQUEST, method);
 
@@ -97,47 +117,62 @@ client & client::set_receive_cb(std::function<void (std::string &)> cb)
 
 http_response client::get_request(const std::string path)
 {
-    return http_response(std::bind(&client::curl_abstract_process,
-                                   this,
-                                   path,
-                                   "GET",
-                                   std::placeholders::_1));
+    if (this->receive_cb) {
+        std::ostringstream str;
+        return http_response::chunk_call(std::bind(&client::curl_abstract_process,
+                                                   this, path, "GET", &str))
+            .set_result(str.str());
+    }
+    return http_response::mono_call(std::bind(&client::curl_abstract_process,
+                                              this, path, "GET", std::placeholders::_1));
 }
 
 http_response client::put_request(const std::string path)
 {
-    return http_response(std::bind(&client::curl_abstract_process,
-                                   this,
-                                   path,
-                                   "PUT",
-                                   std::placeholders::_1));
+    if (this->receive_cb) {
+        std::ostringstream str;
+        return http_response::chunk_call(std::bind(&client::curl_abstract_process,
+                                                   this, path, "PUT", &str))
+            .set_result(str.str());
+    }
+    return http_response::mono_call(std::bind(&client::curl_abstract_process,
+                                              this, path, "PUT", std::placeholders::_1));
 }
 
 http_response client::delete_request(const std::string path)
 {
-    return http_response(std::bind(&client::curl_abstract_process,
-                                   this,
-                                   path,
-                                   "DELETE",
-                                   std::placeholders::_1));
+    if (this->receive_cb) {
+        std::ostringstream str;
+        return http_response::chunk_call(std::bind(&client::curl_abstract_process,
+                                                   this, path, "DELETE", &str))
+            .set_result(str.str());
+    }
+    return http_response::mono_call(std::bind(&client::curl_abstract_process,
+                                              this, path, "DELETE", std::placeholders::_1));
 }
 
 http_response client::post_request(const std::string path)
 {
-    return http_response(std::bind(&client::curl_abstract_process,
-                                   this,
-                                   path,
-                                   "POST",
-                                   std::placeholders::_1));
+    if (this->receive_cb) {
+        std::ostringstream str;
+        return http_response::chunk_call(std::bind(&client::curl_abstract_process,
+                                                   this, path, "POST", &str))
+            .set_result(str.str());
+    }
+    return http_response::mono_call(std::bind(&client::curl_abstract_process,
+                                              this, path, "POST", std::placeholders::_1));
 }
 
 http_response client::patch_request(const std::string path)
 {
-    return http_response(std::bind(&client::curl_abstract_process,
-                                   this,
-                                   path,
-                                   "PATCH",
-                                   std::placeholders::_1));
+    if (this->receive_cb) {
+        std::ostringstream str;
+        return http_response::chunk_call(std::bind(&client::curl_abstract_process,
+                                                   this, path, "PATCH", &str))
+            .set_result(str.str());
+    }
+    return http_response::mono_call(std::bind(&client::curl_abstract_process,
+                                              this, path, "PATCH", std::placeholders::_1));
 }
 
 }
--- http_client/http_response.cc
+++ http_client/http_response.cc
@@ -3,12 +3,40 @@
 namespace navagraha {
 namespace http_client {
 
-http_response::http_response(std::function<long (std::ostringstream &)> caller)
+http_response::http_response()
 {
-    std::ostringstream oss;
-    this->response_code = caller(oss);
-    this->result_str = oss.str();
+
 }
+
+http_response::~http_response()
+{
+
+}
+
+http_response http_response::mono_call(std::function<long (std::ostringstream *)> caller)
+{
+    http_response resp;
+    std::ostringstream oss;
+    resp.response_code = caller(&oss);
+    resp.result_str = oss.str();
+
+    return resp;
+}
+
+http_response http_response::chunk_call(std::function<long ()> caller)
+{
+    http_response resp;
+    resp.response_code = caller();
+    return resp;
+}
+
+http_response & http_response::set_result(std::string str)
+{
+    this->result_str = str;
+
+    return *this;
+}
+
 
 http_response & http_response::response_switch()
 {
