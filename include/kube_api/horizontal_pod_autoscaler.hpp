94b82245b7bc6611d1a28054f026fc63987658af
Gscienty <gaoxiaochuan@hotmail.com>
2019-07-23 06:23:17 -0400

    feat: add hpa api

---
+++ include/kube_api/horizontal_pod_autoscaler.hpp
@@ -0,0 +1,27 @@
+#ifndef _NAVAGRAHA_KUBE_API_HORIZONTAL_POD_AUTOSCALER_H
+#define _NAVAGRAHA_KUBE_API_HORIZONTAL_POD_AUTOSCALER_H
+
+#include "http_client/client.hpp"
+#include "http_client/http_response.hpp"
+#include "kubeent/horizontal_pod_autoscaler.hpp"
+#include "kubeent/status.hpp"
+
+namespace navagraha {
+namespace kube_api {
+
+class horizontal_pod_autoscaler : public http_client::client {
+public:
+    horizontal_pod_autoscaler(CURL * curl, const std::string host);
+
+    http_client::http_response create(const std::string namespace_, kubeent::horizontal_pod_autoscaler & hpa);
+
+    http_client::http_response delete_(const std::string namespace_, const std::string name);
+
+    http_client::http_response read(const std::string namespace_, const std::string name);
+
+    http_client::http_response list(const std::string namespace_);
+}; 
+}
+}
+
+#endif
---
+++ kube_api/horizontal_pod_autoscaler.cc
@@ -0,0 +1,46 @@
+#include "kube_api/horizontal_pod_autoscaler.hpp"
+
+namespace navagraha {
+namespace kube_api {
+
+
+horizontal_pod_autoscaler::horizontal_pod_autoscaler(CURL * curl, const std::string host)
+    : http_client::client(curl, host)
+{
+
+}
+
+http_client::http_response horizontal_pod_autoscaler::create(const std::string namespace_, kubeent::horizontal_pod_autoscaler & hpa)
+{
+    this->set_payload(hpa);
+
+    return this->post_request("/apis/autoscaling/v1/namespaces/"
+                              + namespace_
+                              + "/horizontalpodautoscalers");
+}
+
+http_client::http_response horizontal_pod_autoscaler::delete_(const std::string namespace_, const std::string name)
+{
+    return this->delete_request("/apis/autoscaling/v1/namespaces/"
+                                + namespace_
+                                + "/horizontalpodautoscalers/"
+                                + name);
+}
+
+http_client::http_response horizontal_pod_autoscaler::read(const std::string namespace_, const std::string name)
+{
+    return this->get_request("/apis/autoscaling/v1/namespaces/"
+                             + namespace_
+                             + "/horizontalpodautoscalers/"
+                             + name);
+}
+
+http_client::http_response horizontal_pod_autoscaler::list(const std::string namespace_)
+{
+    return this->get_request("/apis/autoscaling/v1/namespaces/"
+                             + namespace_
+                             + "/horizontalpodautoscalers");
+}
+
+}
+}
