4f2caa5b77e05ed5e501e331c9f1a5c24838d53c
Gscienty <gaoxiaochuan@hotmail.com>
2019-08-12 23:39:51 -0400

    feat: add func_up_cfg && create statelessness func

--- jni/func.cc
+++ jni/func.cc
@@ -2,6 +2,7 @@
 #include "jni/string.hpp"
 #include "cli/config.hpp"
 #include "args/func_up.hpp"
+#include "process/func.hpp"
 
 static void jconfig_to_config(JNIEnv * env, navagraha::cli::config * cfg, jobject jcfg);
 static void j_func_up_to_func_up_arg(JNIEnv * env, navagraha::args::func_up * func_up_cfg, jobject jcfg);
@@ -18,6 +19,8 @@ JNIEXPORT jstring JNICALL
 
     jconfig_to_config(env, &cfg, j_cfg);
     j_func_up_to_func_up_arg(env, &func_up_arg, j_func_up);
+
+    navagraha::process::func(cfg).up(func_up_arg);
 
     return str2jstring(env, std::string("created"));
 }
@@ -51,9 +54,23 @@ static void j_func_up_to_func_up_arg(JNIEnv * env, navagraha::args::func_up * fu
     jmethodID policy_mid = env->GetMethodID(arg_cls, "getPolicy", "()Ljava/lang/String;");
     jmethodID image_mid = env->GetMethodID(arg_cls, "getImage", "()Ljava/lang/String;");
     jmethodID namespace_mid = env->GetMethodID(arg_cls, "getNamespace", "()Ljava/lang/String;");
-    jmethodID stateful_mid = env->GetMethodID(arg_cls, "getStateful", "()Z;");
-    jmethodID update_mid = env->GetMethodID(arg_cls, "getUpdate", "()Z;");
+    jmethodID stateful_mid = env->GetMethodID(arg_cls, "getStateful", "()Z");
+    jmethodID update_mid = env->GetMethodID(arg_cls, "getUpdate", "()Z");
     jmethodID replicas_mid = env->GetMethodID(arg_cls, "getReplicas", "()I");
 
-    // TODO
+    jstring name_jstr = static_cast<jstring>(env->CallObjectMethod(jent, name_mid));
+    jstring policy_jstr = static_cast<jstring>(env->CallObjectMethod(jent, policy_mid));
+    jstring image_jstr = static_cast<jstring>(env->CallObjectMethod(jent, image_mid));
+    jstring namespace_jstr = static_cast<jstring>(env->CallObjectMethod(jent, namespace_mid));
+    jboolean stateful_jbool = env->CallBooleanMethod(jent, stateful_mid);
+    jboolean update_jbool = env->CallBooleanMethod(jent, update_mid);
+    jint replicas_jint = env->CallIntMethod(jent, replicas_mid);
+
+    func_up_cfg->name = jstring2str(env, name_jstr);
+    func_up_cfg->policy = jstring2str(env, policy_jstr);
+    func_up_cfg->namespace_ = jstring2str(env, namespace_jstr);
+    func_up_cfg->image = jstring2str(env, image_jstr);
+    func_up_cfg->stateful = stateful_jbool;
+    func_up_cfg->update = update_jbool;
+    func_up_cfg->replicas = replicas_jint;
 }
