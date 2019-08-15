bb8b8b6f35a571036afd01055f4f7bd50b7235f1
Gscienty <gaoxiaochuan@hotmail.com>
2019-08-15 03:20:04 -0400

    package add depen

--- dashboard/fe/src/App.js
+++ dashboard/fe/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import {
+    fetchSystemInfoNavaVersion,
+    SYSTEM_INFO_NAVA_VERSION_UNSET
+} from './actions/system_info'
 
-function App() {
-  return (
-    <div className="App">
-        Hello Nava
-    </div>
-  );
-}
+class App extends React.Component {
 
-export default App;
+    componentWillMount() {
+        if (this.props.navaVersion.state === SYSTEM_INFO_NAVA_VERSION_UNSET) {
+            this.props.dispatch(fetchSystemInfoNavaVersion());
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                Hello { this.props.navaVersion.name } { this.props.navaVersion.version }
+            </div>
+        );
+    }
+};
+
+export default connect(({ systemInfo }) => ({
+    navaVersion: systemInfo.navaVersion
+}))(App);
--- dashboard/fe/src/index.js
+++ dashboard/fe/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './reducer';
 
 ReactDOM.render((
-    <HashRouter>
-        <Switch>
-            <Route path="/" component={App} />
-        </Switch>
-    </HashRouter>
+    <Provider store={store}>
+        <HashRouter>
+            <Switch>
+                <Route path="/" component={App} />
+            </Switch>
+        </HashRouter>
+    </Provider>
 ), document.getElementById('root'));
 
 serviceWorker.unregister();
